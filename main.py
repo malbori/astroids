6d77191857241b48edfc981ca063d7de6da35155
malbori <mahdialbori@gmail.com>
2025-02-02 16:00:27 -0500

    added player object and circleshape class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 # from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_SPAWN_RATE
 from constants import *
@@ -7,12 +8,22 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player_start_pos = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    player = Player(player_start_pos[0], player_start_pos[1])
 
     while True:
         screen.fill(color="black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.draw(screen=screen)
+        delta = game_clock.tick(60)
+        dt = delta / 1000
+
         pygame.display.flip()
 
 
